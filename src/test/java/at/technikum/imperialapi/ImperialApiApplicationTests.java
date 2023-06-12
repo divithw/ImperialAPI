0e07f4e29af10d24093e9cec080691fe687f1e74
divithw <divith.wijesekera@outlook.com>
2023-06-12 19:10:45 +0200

    Initial commit

---
+++ src/test/java/at/technikum/imperialapi/ImperialApiApplicationTests.java
@@ -0,0 +1,13 @@
+package at.technikum.imperialapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImperialApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
